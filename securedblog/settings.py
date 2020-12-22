8422bf2980498cf519665288278239cc7306c1e8
Ted Peters <44210809+Drop-G@users.noreply.github.com>
2020-12-22 17:29:28 -0500

    syntax error fixed

--- securedblog/settings.py
+++ securedblog/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 import os
 import environ
-import django-heroku
 
 env = environ.Env(
     # set casting, default value
