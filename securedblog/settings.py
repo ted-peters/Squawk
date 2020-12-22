095a0365d880d2953b2277c9aace996e219b740f
Ted Peters <44210809+Drop-G@users.noreply.github.com>
2020-12-22 17:19:39 -0500

    import django-heroku

--- securedblog/settings.py
+++ securedblog/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import environ
+import django-heroku
 
 env = environ.Env(
     # set casting, default value
