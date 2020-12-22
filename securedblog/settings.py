502de1faa689556bbd69d833364c7d0a5237bbdd
Ted Peters <44210809+Drop-G@users.noreply.github.com>
2020-12-22 15:47:37 -0500

    fixed, now to set up S3 bucket

--- securedblog/settings.py
+++ securedblog/settings.py
@@ -67,6 +67,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'fontawesome-free'
 ]
 
 MIDDLEWARE = [
@@ -158,3 +159,10 @@ MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = 'security-home'
 
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env('EMAIL_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_PASS')
