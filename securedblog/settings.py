8be8493cf35d9809c0433db9f8de2e843592d119
Ted Peters <44210809+Drop-G@users.noreply.github.com>
2020-12-22 16:53:25 -0500

    photo storage is an issue but evertything else is working

--- securedblog/settings.py
+++ securedblog/settings.py
@@ -67,7 +67,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'fontawesome-free'
+    'fontawesome-free',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -159,6 +160,17 @@ MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = 'security-home'
 
 LOGIN_URL = 'login'
+
+# AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+# AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+# AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+# AWS_DEFAULT_ACL = 'public-read'
+# AWS_S3_FILE_OVERWRITE = False
+# AWS_DEFAULT_ACL = None
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# AWS_S3_REGION_NAME = 'us-east-2'
+# AWS_S3_SIGNATURE_VERSION = 's3v4'
+# AWS_QUERYSTRING_AUTH=False
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
